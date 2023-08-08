93b8413d99c33bd3a3cdf0438d8d555f90f3d9f1
NalawadeAnil14 <55385603+NalawadeAnil14@users.noreply.github.com>
2023-08-08 22:53:36 +0530

    Added codedeploy-agent installtion command

--- beforeInstall.sh
+++ beforeInstall.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+
+sudo yum update -y
+sudo yum install -y ruby wget
+cd /home/ec2-user
+wget https://aws-codedeploy-<region>.s3.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+
+sudo service codedeploy-agent start
+sudo chkconfig codedeploy-agent on
+
 cd /var/www/html
 sudo rm -rf *
